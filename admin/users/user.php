c3642d756d0017bf03cafdcd3b467e8a1b0aa608
PierceWind <PierceWind@outlook.com>
2023-09-25 05:03:14 +0800

    page title

--- admin/users/user.php
+++ admin/users/user.php
@@ -43,7 +43,7 @@ if (isset($_POST['delete_rec'])) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Admin Dashboard</title>
+        <title>Manage Users</title>
         <link rel="icon" type="image/x-icon" href="../../files/icons/tdf.png">
         <link rel="stylesheet" type="text/css" href="../style.css">
     </head>
