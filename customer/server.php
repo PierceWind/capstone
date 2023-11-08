e8da0330bf515ffbf7f3921dc02576a715251c64
PierceWind <PierceWind@outlook.com>
2023-11-08 16:15:59 +0800

    save to orders table in addtocart is okay

--- customer/server.php
+++ customer/server.php
@@ -1,48 +1,62 @@
-<?php    
-    
-     // Get the cart data sent from the client
-    
-    // Database configuration
-    $servername = "localhost";  // Replace with your database server name
-    $username = "root";  // Replace with your database username
-    $password = "xoxad";  // Replace with your database password
-    $dbname = "capstone";  // Replace with your database name
-    $conn = new mysqli($servername, $username, $password, $dbname);
+<?php
+$servername = "localhost";  // Replace with your database server name
+$username = "root";  // Replace with your database username
+$password = "xoxad";  // Replace with your database password
+$dbname = "capstone";  // Replace with your database name
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check the connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
-    $cartData = json_decode($_POST['cartArray'], true);
-    // Insert the order into the 'orders' table
-    $orderInsertSQL = "INSERT INTO orders (OrderDateTime, OrderStatus, QueueNumber) VALUES (NOW(), 'Processing', NULL)";
-
-    if ($conn->query($orderInsertSQL) === true) {
-        // Get the auto-generated OrderID
-        $orderID = $conn->insert_id;
-
-        // Insert each item in the cart into the 'order_items' table, associating them with the OrderID
-        foreach ($cart as $item) {
-            $productID = $conn->real_escape_string($item['ProductID']);
-            $quantity = $item['Quantity'];
-            $price = $item['Price'];
-
-            $itemInsertSQL = "INSERT INTO order_items (OrderID, ProductID, Quantity, Subtotal) VALUES ($orderID, '$productID', $quantity, $price)";
-
-            if ($conn->query($itemInsertSQL) !== true) {
-                die("Error inserting order item: " . $conn->error);
-            }
-        }
-
-        echo "Order placed successfully!";
-    } else {
-        die("Error placing the order: " . $conn->error);
-    
-    }
-     // Close the database connection
-    $conn->close();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $cartData = json_decode($_POST['cart'], true);
+
+    // Start a transaction to ensure data consistency
+    $conn->begin_transaction();
+
+    // Find the current maximum QueueNumber for non-Queued orders
+    $queueNumberQuery = "SELECT COALESCE(MAX(QueueNumber), 0) AS maxQueueNumber FROM orders WHERE OrderStatus = 'Queued'";
+    $queueNumberResult = $conn->query($queueNumberQuery);
+
+    if ($queueNumberResult) {
+        $row = $queueNumberResult->fetch_assoc();
+        $maxQueueNumber = $row['maxQueueNumber'];
+
+        // Check if the maximum QueueNumber is 50, and if not, increment it; otherwise, reset it to 1
+        if ($maxQueueNumber < 50) {
+            $newQueueNumber = $maxQueueNumber + 1;
+        } else {
+            $newQueueNumber = 1;
+        }
+
+        // Insert the order with the determined QueueNumber
+        $orderInsertSQL = "INSERT INTO orders (OrderDateTime, orderStatus, queueNumber) VALUES (NOW(), 'Queued', $newQueueNumber)";
+        if ($conn->query($orderInsertSQL) === true) {
+            $orderID = $conn->insert_id;
+
+            foreach ($cartData as $item) {
+                $productID = $conn->real_escape_string($item['ProductID']);
+                $quantity = $item['Quantity'];
+                $price = $item['Price'];
+
+                $itemInsertSQL = "INSERT INTO order_items (OrderID, ProductID, Quantity, Subtotal) VALUES ($orderID, '$productID', $quantity, $price)";
+
+                if ($conn->query($itemInsertSQL) !== true) {
+                    die("Error inserting order item: " . $conn->error);
+                }
+            }
+
+            echo "Order placed successfully!";
+            $conn->commit(); // Commit the transaction
+        } else {
+            $conn->rollback(); // Rollback the transaction in case of an error
+            die("Error placing the order: " . $conn->error);
+        }
+    } else {
+        die("Error retrieving QueueNumber: " . $conn->error);
+    }
+
+    $conn->close();
+}
 ?>
