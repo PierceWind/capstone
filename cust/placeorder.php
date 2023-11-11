c501f4356047c8fb266759529f803346c4f70cf1
PierceWind <PierceWind@outlook.com>
2023-11-12 02:07:44 +0800

    placeorder style is updated

--- cust/placeorder.php
+++ cust/placeorder.php
@@ -1,5 +1,3 @@
-<?=template_header('Place Order')?>
-
 <?php
 // Check if the "Back to Home" link is clicked
 if (isset($_POST['back_to_home'])) {
@@ -12,14 +10,74 @@ if (isset($_POST['back_to_home'])) {
 }
 ?>
 
-<div class="placeorder content-wrapper">
-    <h1>Your Order Has Been Placed</h1>
-    <p>Order Number: <?= isset($_SESSION['orderId']) ? $_SESSION['orderId'] : 'N/A' ?></p>
-    <p>Queue Number: <?= isset($_SESSION['queueNumber']) ? $_SESSION['queueNumber'] : 'N/A' ?></p>
-    <p>Thank you for ordering with us! We'll contact you by email with your order details.</p>
-    <form method="post">
-        <input type="submit" name="back_to_home" value="Back to Home">
-    </form>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thank You Page</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap">
+    <style>
+        body {
+            background: #700202;
+            color: white;
+            margin: 0; /* Remove default margin */
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh; /* Ensure the body takes at least the full viewport height */
+        }
 
-<?=template_footer()?>
+        .placeorder {
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 40px;
+            margin: 20px 0; /* Add some margin for spacing */
+            /* Add any additional styling for your h1 element here */
+            font-family: 'Great Vibes', cursive; /* Use the Google Font 'Great Vibes' */
+        }
+
+        p {
+            font-size: 30px;
+        }
+
+        .back-to-home-button {
+            margin-left: 5px;
+            padding: 12px 20px;
+            border: 0;
+            border-radius: 20px;
+            background: yellow;
+            color: black;
+            font-size: 30px;
+            font-weight: bold;
+            cursor: pointer;
+            border-radius: 10px;
+            margin-top: 20px; /* Add some top margin for spacing */
+            display: inline-block;
+        }
+
+        .back-to-home-button:hover {
+            background: yellow;
+            color: black;
+        }
+    </style>
+</head>
+<body>
+    <div class="placeorder content-wrapper">
+        
+        <h1>Thank You For Your Order</h1>
+        <hr>
+        <strong>
+            <p>Order Number: <?= isset($_SESSION['orderId']) ? $_SESSION['orderId'] : 'N/A' ?></p>
+            <p>Queue Number: <?= isset($_SESSION['queueNumber']) ? $_SESSION['queueNumber'] : 'N/A' ?></p>
+        </strong>
+        <hr>
+        <h1>Bon Appetite</h1>
+        <form method="post" class="placeorder">
+            <input type="submit" name="back_to_home" value="ORDER NOW" class="back-to-home-button">
+        </form>
+    </div>
+</body>
+</html>
