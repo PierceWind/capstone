ef86b1fe12f8910ae527f78abac252c5b7c84264
Jenshi <jennylynv17@gmail.com>
2023-11-15 01:02:21 +0800

    semi fixed the category item

--- cust/filtercategories.php
+++ cust/filtercategories.php
@@ -15,17 +15,17 @@ try {
 
     // Use a prepared statement to prevent SQL injection
     $stmt = $pdo->prepare('SELECT product.*, prodimage.productImg, inventory.stock 
-                           FROM product 
-                           LEFT JOIN prodimage ON product.prodId = prodimage.productId
-                           LEFT JOIN inventory ON product.prodId = inventory.prodCode 
-                           WHERE prodCategory = :category');
+                        FROM product 
+                        LEFT JOIN prodimage ON product.prodId = prodimage.productId
+                        LEFT JOIN inventory ON product.prodId = inventory.prodCode 
+                        WHERE prodCategory = :category');
     $stmt->bindParam(':category', $category, PDO::PARAM_STR);
     $stmt->execute();
 
     // Fetch the items
     $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Generate HTML representation of the items
+    // Generate HTML representation of the itemsa
     echo '<div class="products">';
     foreach ($items as $item) {
         // Check if the 'stock' key exists in the $item array
@@ -39,23 +39,21 @@ try {
         if ($stock == 0) {
             echo '<div class="unavailable-message">UNAVAILABLE</div>';
         }
-        
 
         echo '<a href="index.php?page=product&id=' . $item['prodId'] . '" class="product-link">';
         echo '<div class="product-image-container">';
         echo '<img src="' . $extension . $productImg . '" alt="' . htmlspecialchars($item['prodName']) . '">';
-        echo '</div>';
-        echo '<span class="name" style="color: #700202;"><strong>' . htmlspecialchars($item['prodName']) . '</strong></span><br>';
+        echo '<span class="name" style="color: #700202;"><strong>' . htmlspecialchars($item['prodName']) . '</strong></span>';
         echo '<span class="price">&#8369;' . htmlspecialchars($item['prodPrice']) . '</span><br>';
         echo '</a>';
         echo '</div>';
+        }
+        echo '</div>';
+    } catch (PDOException $e) {
+        // Handle database connection or query errors
+        echo 'Error: ' . $e->getMessage();
+    } finally {
+        // Close the database connection
+        $pdo = null;
     }
-    echo '</div>';
-} catch (PDOException $e) {
-    // Handle database connection or query errors
-    echo 'Error: ' . $e->getMessage();
-} finally {
-    // Close the database connection
-    $pdo = null;
-}
 ?>
